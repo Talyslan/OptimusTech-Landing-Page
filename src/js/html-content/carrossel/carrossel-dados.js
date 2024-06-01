883c14b408ece61428ef14479ccc92052f930131
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2024-06-01 02:14:20 -0300

    add: inicio do carrossel (tive que salvar)

---
+++ src/js/html-content/carrossel/carrossel-dados.js
@@ -0,0 +1,51 @@
+const carrossel = [
+    {
+        name: 'Júlia Castro',
+        position: 'Desenvolvedora Web',
+        avatar: 'avatar1.jpg',
+        message: 'OptimusTech se importa com a saúde dos seus colaboradores e sempre procura nos dar todo tipo de auxílio possível.'
+    },
+    {
+        name: 'Carlos Silva',
+        position: 'Engenheiro de Software',
+        avatar: 'avatar2.jpg',
+        message: 'A empresa oferece um ambiente acolhedor e sempre incentiva o desenvolvimento pessoal e profissional.'
+    },
+    {
+        name: 'Ana Paula',
+        position: 'Designer Gráfico',
+        avata: 'avatar3.jpg',
+        message: 'Aqui na OptimusTech temos a liberdade de propor novas ideias e participar de projetos inovadores.'
+    },
+    {
+        name: 'Miguel Santos',
+        position: 'Gerente de Projetos',
+        avatar: 'avatar4.jpg',
+        message: 'A preocupação com o bem-estar dos funcionários é evidente e isso reflete na nossa produtividade.'
+    },
+    {
+        name: 'Larissa Oliveira',
+        position: 'Analista de Dados',
+        avatar: 'avatar5.jpg',
+        message: 'Trabalhar na OptimusTech tem sido uma experiência enriquecedora, com muitas oportunidades de aprendizado.'
+    },
+    {
+        name: 'Pedro Lima',
+        position: 'Desenvolvedor Front-end',
+        avatar: 'avatar6.jpg',
+        message: 'A cultura de inclusão e respeito faz toda a diferença no ambiente de trabalho.'
+    }
+]
+
+/* HTML Template
+<div class="item">
+    <p>OptimusTech se importa com a saúde dos seus colaboradores e sempre procura nos dar todo tipo de auxílio possível.</p>
+    <div class="user">
+        <img src="./public/people/avatar1.jpg" alt="">
+        <span>Júlia Castro</span>
+        <span>Desenvolvedora Web</span>
+    </div>
+</div>
+*/
+
+export default carrossel;
---
+++ src/js/html-content/carrossel/carrossel.js
@@ -0,0 +1,22 @@
+// list object 
+import carrossel from "./carrossel-dados.js";
+console.log(carrossel)
+// selectors
+const carrossel_html = document.querySelector('#carrossel');
+
+// function
+const createCarrosselElement = ({ name, position, avatar, message }) => {
+    return `
+        <div class="item">
+            <p>${message}</p>
+            <div class="user">
+                <img src="./public/people/${avatar}" alt="">
+                <span>${name}</span>
+                <span>${position}</span>
+            </div>
+        </div>
+    `;
+};
+
+// inner content
+carrossel.forEach(obj => carrossel_html.innerHTML += createCarrosselElement(obj));
